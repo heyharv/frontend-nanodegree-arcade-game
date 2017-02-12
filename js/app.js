3f307bdbe94c17b2b896632a48d6c4272c954926
System Administrator <root@Harveys-MacBook-Air-2.local>
2017-02-12 11:33:02 -0800

    eleventh commite, Enemies can move

--- js/app.js
+++ js/app.js
@@ -17,12 +17,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    /*
     this.x += this.speed * dt;
     if(this.x > 505) {
         this.x = Math.random()*100;
     }
-    */
 };
 
 // Draw the enemy on the screen, required method for game
