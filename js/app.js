951c432f833c668804f05449bf61bbc4bbf95b06
System Administrator <root@Harveys-MacBook-Air-2.local>
2017-02-11 18:53:25 -0800

    sixth commit

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.x = x;
+    this.y = y;
+    this.speed = 50;
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -28,13 +31,16 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function() {
+var Player = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.speed = 100;
     this.sprite = 'images/char-boy.png';
 };
 
 Player.prototype.update = function(dt) {
     this.x += this.speed * dt;
-    if(this.x > 606) {
+    if(this.x > 505) {
         this.x = Player.reset();
     }
 };
@@ -44,15 +50,23 @@ Player.prototype.render = function() {
 };
 
 Player.prototype.handleInput = function() {
-
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    } else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
 };
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [new Enemy(10, 10)];
-var player = new Player();
+var allEnemies = [];
+var enemy = new Enemy(101, 166);
+var player = new Player(303, 498);
+
+var rightPressed = false;
+var leftPressed = false;
 
 
 
